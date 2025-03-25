50eb43ca4b282afb0b97c4669900a95018f85ef0
Natan Herzog <collegenatan@gmail.com>
2025-03-24 22:04:13 -0400

    all new results

--- generate_individual_bricks.m
+++ generate_individual_bricks.m
@@ -6,6 +6,8 @@ function generate_individual_bricks(REFINE_SCALE, EXPORT_TOGGLE, GRAPH_TOGGLE, S
     STEP_TYPE string {mustBeText} = 'STEP1'
   end
 
+  addpath("submodules\distmesh-utilities\")
+
   %* --------------- BRICK PARAMETERS --------------- *%
   GPHS_WIDTH_x = 3.92;            % [in]
   GPHS_DEPTH_y = 3.668;           % [in]
@@ -28,7 +30,7 @@ function generate_individual_bricks(REFINE_SCALE, EXPORT_TOGGLE, GRAPH_TOGGLE, S
                                    GPHS_WIDTH_x/2, GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2);...
                                    GPHS_WIDTH_x/2, GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1);...
                                   -GPHS_WIDTH_x/2, GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1)];
-  x_z_positive_y_face_mesh = generate_rectangle(x_z_positive_y_face_vertices, REFINE_SCALE);
+  x_z_positive_y_face_mesh = generateRectangle(x_z_positive_y_face_vertices, REFINE_SCALE);
   x_z_positive_y_face_mesh = flipNormals(x_z_positive_y_face_mesh);
   x_z_negative_y_face_mesh = flipNormals(translateMesh(x_z_positive_y_face_mesh,[0,-GPHS_DEPTH_y,0]));
 
@@ -37,7 +39,7 @@ function generate_individual_bricks(REFINE_SCALE, EXPORT_TOGGLE, GRAPH_TOGGLE, S
                                   GPHS_WIDTH_x/2, -GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2);...
                                   GPHS_WIDTH_x/2, -GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1);...
                                   GPHS_WIDTH_x/2,  GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1)];
-  y_z_positive_x_face_mesh = generate_rectangle(y_z_positive_x_face_vertices, REFINE_SCALE);
+  y_z_positive_x_face_mesh = generateRectangle(y_z_positive_x_face_vertices, REFINE_SCALE);
   y_z_positive_x_face_mesh = flipNormals(y_z_positive_x_face_mesh);
   y_z_negative_x_face_mesh = flipNormals(translateMesh(y_z_positive_x_face_mesh,[-GPHS_WIDTH_x,0,0]));
 
@@ -67,32 +69,6 @@ function generate_individual_bricks(REFINE_SCALE, EXPORT_TOGGLE, GRAPH_TOGGLE, S
   for i = 1 : (GPHS_STACK_COUNT) - 1
     BRICK_meshes{GPHS_STACK_COUNT - i, 1} = translateMesh(GPHS_bricks_mesh, [0, 0, i*(GPHS_HEIGHT_z)]);
   end
-
-  % TODO add top face for row 1 (facing positive z)
-  % TODO add bottom face for row 18 (facing negative z)
-
-  x_y_plane_mesh = generate_rectangle( [ -GPHS_WIDTH_x/2 , -GPHS_DEPTH_y/2, 0;...
-                                         GPHS_WIDTH_x/2 , -GPHS_DEPTH_y/2, 0;...
-                                         GPHS_WIDTH_x/2 ,  GPHS_DEPTH_y/2, 0;...
-                                        -GPHS_WIDTH_x/2 ,  GPHS_DEPTH_y/2, 0;] , REFINE_SCALE );  % facing pos z
-
-  top_face_mesh = translateMesh(x_y_plane_mesh, [0,0,GPHS_STACK_COUNT/2 * GPHS_HEIGHT_z]);
-  bottom_face_mesh = flipNormals(translateMesh(x_y_plane_mesh, [0,0,-GPHS_STACK_COUNT/2 * GPHS_HEIGHT_z]));
-
-  top_brick_mesh = BRICK_meshes{1, 1};
-  top_brick_mesh_points = [ top_brick_mesh.Points; top_face_mesh.Points ];
-  top_brick_mesh_connectivity = [ top_brick_mesh.ConnectivityList; top_face_mesh.ConnectivityList + size(top_brick_mesh.Points,1) ];
-
-  top_brick_mesh = triangulation(top_brick_mesh_connectivity, top_brick_mesh_points);
-
-  bottom_brick_mesh = BRICK_meshes{GPHS_STACK_COUNT, 1};
-  bottom_brick_mesh_points = [ bottom_brick_mesh.Points; bottom_face_mesh.Points ];
-  bottom_brick_mesh_connectivity = [ bottom_brick_mesh.ConnectivityList; bottom_face_mesh.ConnectivityList + size(bottom_brick_mesh.Points,1) ];
-
-  bottom_brick_mesh = triangulation(bottom_brick_mesh_connectivity, bottom_brick_mesh_points);
-
-  BRICK_meshes{1, 1} = top_brick_mesh;
-  BRICK_meshes{GPHS_STACK_COUNT, 1} = bottom_brick_mesh;
 
   %* --------------- EXPORT MESHES --------------- *%
   if EXPORT_TOGGLE
--- plot_results.m
+++ plot_results.m
@@ -22,155 +22,139 @@ output_path = "pdfs/";
 paper_size = 12;
 line_width = 1;
 marker_size = 5;
-line_style = ":";
+line_style = "-";
 plot_aspect_ratio = [1,1,1];
 
 range_1 = 1:5;
 range_2 = 6:11;
 range_3 = 12:18;
 
-% for i = 1 : 18
-%   % figure(1)
-%   semilogy( 1:18 , results(:,i,1) , "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   hold on
-%   semilogy( 1:18 , results(:,i,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( 1:18 , results(:,i,3), "*", "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( 1:18 , results(:,i,4), "s", "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   xlabel("Brick", "Interpreter", "latex")
-%   ylabel("$F_{ij}$", "Interpreter", "latex")
-%   legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
-%   xlim([1,18])
-%   xticks([1,9,18])
-%   xticklabels([1,9,18])
-%   ylim([1e-8, 1e-1])
-%   yticks([1e-8, 1e-6, 1e-3, 1e-1])
-%   pbaspect(plot_aspect_ratio)
-%   % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
-%   % title( title_text , "Interpreter", "latex" )
-%   set(gca, "FontSize", 18)
-% 
-% 
-%   % set(gcf,'Units','centimeters')
-%   % set(gcf,'PaperUnits','centimeters')
-%   % set(gcf,'PaperSize',[paper_size/2, paper_size])
-%   % set(gcf,'InnerPosition',[0.5,0.5,paper_size-1,paper_size-1])
-%   % set(gcf,'OuterPosition',[0,0,paper_size/2,paper_size])
-%   % set(gcf,'PaperPosition',[0,0, paper_size/2, paper_size])
-% 
-%   hold off
-%   full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + ".pdf";
-%   % print(full_file_name, '-dpdf', '-r2400');
-%   exportgraphics(gcf, full_file_name)
-% 
-% 
-% 
-% 
-%   % figure(2)
-%   semilogy( range_1 , results(range_1,i,1), "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   hold on
-%   semilogy( range_1 , results(range_1,i,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( range_1 , results(range_1,i,3), "*", "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( range_1 , results(range_1,i,4), "s", "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   xlabel("Brick", "Interpreter", "latex")
-%   ylabel("$F_{ij}$", "Interpreter", "latex")
-%   legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
-%   xlim([1,5])
-%   xticks([1,3,5])
-%   xticklabels([1,3,5])
-%   ylim([1e-8, 1e-1])
-%   yticks([1e-8, 1e-6, 1e-3, 1e-1])
-%   pbaspect(plot_aspect_ratio)
-%   % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
-%   % title( title_text , "Interpreter", "latex" )
-%   set(gca, "FontSize", 18)
-%   hold off
-%   full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + "-bricks-" + num2str(range_1(1)) + "-" + num2str(range_1(end)) + ".pdf";
-%   % print(full_file_name, '-dpdf', '-r2400');
-%   exportgraphics(gcf, full_file_name)
-% 
-% 
-% 
-% 
-%   % figure(3)
-%   semilogy( range_2 , results(range_2,i,1), "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   hold on
-%   semilogy( range_2 , results(range_2,i,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( range_2 , results(range_2,i,3), "*", "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( range_2 , results(range_2,i,4), "s", "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   xlabel("Brick", "Interpreter", "latex")
-%   ylabel("$F_{ij}$", "Interpreter", "latex")
-%   legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
-%   xlim([range_2(1), range_2(end)])
-%   xticks([range_2(1), range_2(floor(length(range_2)/2)) range_2(end)])
-%   ylim([1e-8, 1e-1])
-%   yticks([1e-8, 1e-6, 1e-3, 1e-1])
-%   pbaspect(plot_aspect_ratio)
-%   % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
-%   % title( title_text , "Interpreter", "latex" )
-%   set(gca, "FontSize", 18)
-%   hold off
-%   full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + "-bricks-" + num2str(range_2(1)) + "-" + num2str(range_2(end)) + ".pdf";
-%   % print(full_file_name, '-dpdf', '-r2400');
-%   exportgraphics(gcf, full_file_name)
-% 
-% 
-% 
-% 
-%   % figure(4)
-%   semilogy( range_3 , results(range_3,i,1), "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   hold on
-%   semilogy( range_3 , results(range_3,i,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( range_3 , results(range_3,i,3), "*", "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
-%   semilogy( range_3 , results(range_3,i,4), "s", "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
-%   xlabel("Brick", "Interpreter", "latex")
-%   ylabel("$F_{ij}$", "Interpreter", "latex")
-%   legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
-%   xlim([range_3(1), range_3(end)])
-%   xticks([range_3(1), range_3(ceil(length(range_3)/2)) range_3(end)])
-%   ylim([1e-8, 1e-1])
-%   yticks([1e-8, 1e-6, 1e-3, 1e-1])
-%   pbaspect(plot_aspect_ratio)
-%   % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
-%   % title( title_text , "Interpreter", "latex" )
-%   set(gca, "FontSize", 18)
-%   hold off
-%   full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + "-bricks-" + num2str(range_3(1)) + "-" + num2str(range_3(end)) + ".pdf";
-%   % print(full_file_name, '-dpdf', '-r2400');
-%   exportgraphics(gcf, full_file_name)
-% 
-% 
-% end
+marker_1 = "o";
+marker_2 = "+";
+marker_3 = "*";
+marker_4 = "s";
+
+for i = 1 : 18
+  % figure(1)
+  semilogy( 1:18 , results(:,i,1), "Marker", marker_1, "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  hold on
+  semilogy( 1:18 , results(:,i,2), "Marker", marker_2, "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  semilogy( 1:18 , results(:,i,3), "Marker", marker_3, "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  semilogy( 1:18 , results(:,i,4), "Marker", marker_4, "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  xlabel("Brick", "Interpreter", "latex")
+  ylabel("$F_{ij}$", "Interpreter", "latex")
+  legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
+  xlim([1,18])
+  xticks(1:18)
+  xticklabels(["1","","3","","","6","","","9","","","12","","","15","","","18"])
+  set(gca, "XTickLabelRotation", 0)
+  ylim([1e-8, 1e-1])
+  yticks([1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1])
+  yticklabels(["1e-8", "", "1e-6", "", "1e-4", "", "1e-2", "1e-1"])
+  pbaspect(plot_aspect_ratio)
+  grid on
+  % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
+  % title( title_text , "Interpreter", "latex" )
+  set(gca, "FontSize", 18)
+  % set(gca, "XMinorTick", 'on')
+
+  set(gcf,'Units','centimeters')
+  set(gcf,'PaperUnits','centimeters')
+  set(gcf,'PaperSize',[paper_size, paper_size])
+  % set(gcf,'InnerPosition',[0.5,0.5,paper_size-1,paper_size-1])
+  set(gcf,'OuterPosition',[0.1,0.1,paper_size-0.2,paper_size-0.2])
+  set(gcf,'PaperPosition',[0,0, paper_size/2, paper_size])
+
+  hold off
+  full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + ".pdf";
+  % print(full_file_name, '-dpdf', '-r2400');
+  exportgraphics(gcf, full_file_name)
+
+
+
+
+  % % figure(2)
+  % semilogy( range_1 , results(range_1,i,1), "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  % hold on
+  % semilogy( range_1 , results(range_1,i,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  % semilogy( range_1 , results(range_1,i,3), "*", "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  % semilogy( range_1 , results(range_1,i,4), "s", "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  % xlabel("Brick", "Interpreter", "latex")
+  % ylabel("$F_{ij}$", "Interpreter", "latex")
+  % legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
+  % xlim([1,5])
+  % xticks([1,3,5])
+  % xticklabels([1,3,5])
+  % ylim([1e-8, 1e-1])
+  % yticks([1e-8, 1e-6, 1e-3, 1e-1])
+  % pbaspect(plot_aspect_ratio)
+  % grid on
+  % % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
+  % % title( title_text , "Interpreter", "latex" )
+  % set(gca, "FontSize", 18)
+  % hold off
+  % full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + "-bricks-" + num2str(range_1(1)) + "-" + num2str(range_1(end)) + ".pdf";
+  % % print(full_file_name, '-dpdf', '-r2400');
+  % exportgraphics(gcf, full_file_name)
+  % 
+  % 
+  % 
+  % 
+  % % figure(3)
+  % semilogy( range_2 , results(range_2,i,1), "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  % hold on
+  % semilogy( range_2 , results(range_2,i,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  % semilogy( range_2 , results(range_2,i,3), "*", "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  % semilogy( range_2 , results(range_2,i,4), "s", "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  % xlabel("Brick", "Interpreter", "latex")
+  % ylabel("$F_{ij}$", "Interpreter", "latex")
+  % legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
+  % xlim([range_2(1), range_2(end)])
+  % xticks([range_2(1), range_2(floor(length(range_2)/2)) range_2(end)])
+  % ylim([1e-8, 1e-1])
+  % yticks([1e-8, 1e-6, 1e-3, 1e-1])
+  % pbaspect(plot_aspect_ratio)
+  % grid on
+  % % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
+  % % title( title_text , "Interpreter", "latex" )
+  % set(gca, "FontSize", 18)
+  % hold off
+  % full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + "-bricks-" + num2str(range_2(1)) + "-" + num2str(range_2(end)) + ".pdf";
+  % % print(full_file_name, '-dpdf', '-r2400');
+  % exportgraphics(gcf, full_file_name)
+  % 
+  % 
+  % 
+  % 
+  % % figure(4)
+  % semilogy( range_3 , results(range_3,i,1), "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  % hold on
+  % semilogy( range_3 , results(range_3,i,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  % semilogy( range_3 , results(range_3,i,3), "*", "LineStyle", line_style, "Color", [0,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
+  % semilogy( range_3 , results(range_3,i,4), "s", "LineStyle", line_style, "Color", [0,1,0], "MarkerSize", marker_size, "LineWidth", line_width)
+  % xlabel("Brick", "Interpreter", "latex")
+  % ylabel("$F_{ij}$", "Interpreter", "latex")
+  % legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
+  % xlim([range_3(1), range_3(end)])
+  % xticks([range_3(1), range_3(ceil(length(range_3)/2)) range_3(end)])
+  % ylim([1e-8, 1e-1])
+  % yticks([1e-8, 1e-6, 1e-3, 1e-1])
+  % pbaspect(plot_aspect_ratio)
+  % grid on
+  % % title_text = "Bricks to row (" + num2str(i) + ") Shoes";
+  % % title( title_text , "Interpreter", "latex" )
+  % set(gca, "FontSize", 18)
+  % hold off
+  % full_file_name = output_path + "bricks-to-shoes-row-" + num2str(i) + "-bricks-" + num2str(range_3(1)) + "-" + num2str(range_3(end)) + ".pdf";
+  % % print(full_file_name, '-dpdf', '-r2400');
+  % exportgraphics(gcf, full_file_name)
+
+
+end
 
 
 for i = 1 : 18
-  % plot( 1:18 , results(i,:,1) , "o:k", "MarkerSize", marker_size, "LineWidth", line_width)
-  % hold on
-  % plot( 1:18 , results(i,:,2) , "o--k", "MarkerSize", marker_size, "LineWidth", line_width, "MarkerFaceColor", "k")
-  % plot( 1:18 , results(i,:,3), "s:k", "MarkerSize", marker_size, "LineWidth", line_width)
-  % plot( 1:18 , results(i,:,4), "s--k", "MarkerSize", marker_size, "LineWidth", line_width, "MarkerFaceColor", "k")
-  % grid on
-  % xlabel("Shoe", "Interpreter", "latex")
-  % ylabel("$F_{ij}$", "Interpreter", "latex")
-  % legend("x-y", "left", "right", "y-z", "Interpreter", "latex")
-  % xlim([1,18])
-  % if i < 10
-  %   ylim([0,0.015])
-  % end
-  % title_text = "Brick (" + num2str(i) + ") to all Shoes";
-  % title( title_text , "Interpreter", "latex" )
-  % set(gca, "FontSize", 18)
-  % set(gcf,'PaperUnits','centimeters')
-  % set(gcf,'PaperSize',[paper_size paper_size])
-  % set(gcf,'PaperPosition',[0 0 paper_size paper_size])
-  % set(gcf,'PaperOrientation','portrait')
-  % hold off
-  % full_file_name = output_path + "brick-" + num2str(i) + "-to-shoes.pdf";
-  % print(full_file_name, '-dpdf', '-r2400');
 
-
-
-
-  % figure(1)
   semilogy( 1:18 , results(i,:,1) , "o", "LineStyle", line_style, "Color", [1,0,0], "MarkerSize", marker_size, "LineWidth", line_width)
   hold on
   semilogy( 1:18 , results(i,:,2) , "+", "LineStyle", line_style, "Color", [1,0,1], "MarkerSize", marker_size, "LineWidth", line_width)
@@ -180,8 +164,8 @@ for i = 1 : 18
   ylabel("$F_{ij}$", "Interpreter", "latex")
   legend("col 1", "col 2", "col 3", "col 4", "Interpreter", "latex")
   xlim([1,18])
-  xticks([1,9,18])
-  xticklabels([1,9,18])
+  xticks(1:18)
+  xticklabels(["1","","","","","","","","9","","","","","","","","","18"])
   ylim([1e-8, 1e-1])
   yticks([1e-8, 1e-6, 1e-3, 1e-1])
   pbaspect(plot_aspect_ratio)
