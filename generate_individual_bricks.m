47ca106ce38b305385c4976e9899af9af56d858c
Natan Herzog <collegenatan@gmail.com>
2025-04-10 19:07:51 -0400

    updating all meshes to do convergence study

--- generate_individual_bricks.m
+++ generate_individual_bricks.m
@@ -1,97 +1,94 @@
-function generate_individual_bricks(REFINE_SCALE, EXPORT_TOGGLE, GRAPH_TOGGLE, STEP_TYPE)
-  arguments
-    REFINE_SCALE int8 {mustBePositive, mustBeInteger} = 15
-    EXPORT_TOGGLE logical {mustBeNumericOrLogical} = false
-    GRAPH_TOGGLE logical {mustBeNumericOrLogical} = true
-    STEP_TYPE string {mustBeText} = 'STEP1'
-  end
+clear all
+clc
 
-  addpath("submodules\distmesh-utilities\")
+EXPORT_TOGGLE = true;
+GRAPH_TOGGLE = false;
 
-  %* --------------- BRICK PARAMETERS --------------- *%
-  GPHS_WIDTH_x = 3.92;            % [in]
-  GPHS_DEPTH_y = 3.668;           % [in]
-  %! ----- set brick type settings ----- !%
-  if strcmpi(STEP_TYPE, 'step1')
-    GPHS_HEIGHT_z = 2.09;         % [in]
-    GPHS_STACK_COUNT = 18;
-    STEP1 = true; STEP2 = false;
-  elseif strcmpi(STEP_TYPE, 'step2')
-    GPHS_HEIGHT_z = 2.29;         % [in]
-    GPHS_STACK_COUNT = 16;
-    STEP1 = false; STEP2 = true;
-  end
+STEP_TYPE = 'step1';
 
-  output_path = "assets/";
+addpath("submodules\distmesh-utilities\")
 
-  %* --------------- GENERATE BRICK FACES --------------- *%
-  %! generate GPHS brick faces parallel to the x-z plane centered at (0,0,0)
-  x_z_positive_y_face_vertices = [-GPHS_WIDTH_x/2, GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2);...
-                                   GPHS_WIDTH_x/2, GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2);...
-                                   GPHS_WIDTH_x/2, GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1);...
-                                  -GPHS_WIDTH_x/2, GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1)];
-  x_z_positive_y_face_mesh = generateRectangle(x_z_positive_y_face_vertices, REFINE_SCALE);
-  x_z_positive_y_face_mesh = flipNormals(x_z_positive_y_face_mesh);
-  x_z_negative_y_face_mesh = flipNormals(translateMesh(x_z_positive_y_face_mesh,[0,-GPHS_DEPTH_y,0]));
+%* --------------- BRICK PARAMETERS --------------- *%
+GPHS_WIDTH_x = 3.92;            % [in]
+GPHS_DEPTH_y = 3.668;           % [in]
+%! ----- set brick type settings ----- !%
+if strcmpi(STEP_TYPE, 'step1')
+  GPHS_HEIGHT_z = 2.09;         % [in]
+  GPHS_STACK_COUNT = 18;
+  STEP1 = true; STEP2 = false;
+elseif strcmpi(STEP_TYPE, 'step2')
+  GPHS_HEIGHT_z = 2.29;         % [in]
+  GPHS_STACK_COUNT = 16;
+  STEP1 = false; STEP2 = true;
+end
 
-  %! generate bricks faces parallel to the y-z plane centered at (0,0,0)
-  y_z_positive_x_face_vertices = [GPHS_WIDTH_x/2,  GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2);...
-                                  GPHS_WIDTH_x/2, -GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2);...
-                                  GPHS_WIDTH_x/2, -GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1);...
-                                  GPHS_WIDTH_x/2,  GPHS_DEPTH_y/2,  -GPHS_HEIGHT_z*(GPHS_STACK_COUNT/2 - 1)];
-  y_z_positive_x_face_mesh = generateRectangle(y_z_positive_x_face_vertices, REFINE_SCALE);
-  y_z_positive_x_face_mesh = flipNormals(y_z_positive_x_face_mesh);
-  y_z_negative_x_face_mesh = flipNormals(translateMesh(y_z_positive_x_face_mesh,[-GPHS_WIDTH_x,0,0]));
+assets_path = "assets/bricks/";
+
+
+refinement_levels = [ 51 , 101 , 151 ];
+
+for i = 1 : 3
+  
+  output_path = assets_path + "L" + num2str(i) + "/";
+  
+  num_divisions = refinement_levels(i);
+  x_z_negative_y_face_mesh = translateMesh( translateMesh( rotateMesh( generateRectangle2( GPHS_WIDTH_x , GPHS_HEIGHT_z , num_divisions ) , pi/2 , 1 ) , [ -0.5 * GPHS_WIDTH_x , 0 , 0 ] ) , [ 0 , -0.5 * GPHS_DEPTH_y , 0] );
+  x_z_positive_y_face_mesh = flipMeshAboutPlane( x_z_negative_y_face_mesh , [ 0 , 0 , 0 ] , [ 0 , 1 , 0 ] );
+
+  y_z_negative_x_face_mesh = translateMesh( translateMesh( rotateMesh( rotateMesh( generateRectangle2( GPHS_DEPTH_y , GPHS_HEIGHT_z , num_divisions ) , pi/2 , 1 ) , pi/2 , 3 ) , [ 0 , -0.5 * GPHS_DEPTH_y , 0] ) , [ 0.5 * GPHS_WIDTH_x , 0 , 0] );
+  y_z_positive_x_face_mesh = flipMeshAboutPlane( y_z_negative_x_face_mesh , [ 0 , 0 , 0 ] , [ 1 , 0 , 0 ] );
 
   %* --------------- PACKAGE THE MESHES --------------- *%
   GPHS_bricks_mesh_points = [x_z_positive_y_face_mesh.Points;...
-                             x_z_negative_y_face_mesh.Points;...
-                             y_z_positive_x_face_mesh.Points;...
-                             y_z_negative_x_face_mesh.Points];
+      x_z_negative_y_face_mesh.Points;...
+      y_z_positive_x_face_mesh.Points;...
+      y_z_negative_x_face_mesh.Points];
   GPHS_bricks_mesh_connectivity = [x_z_positive_y_face_mesh.ConnectivityList;...
-                                   x_z_negative_y_face_mesh.ConnectivityList + (size(x_z_positive_y_face_mesh.Points, 1));...
-                                   y_z_positive_x_face_mesh.ConnectivityList + (size(x_z_positive_y_face_mesh.Points, 1) +...
-                                                                                size(x_z_negative_y_face_mesh.Points, 1));...
-                                   y_z_negative_x_face_mesh.ConnectivityList + (size(x_z_positive_y_face_mesh.Points, 1) +...
-                                                                                size(x_z_negative_y_face_mesh.Points, 1) +...
-                                                                                size(y_z_positive_x_face_mesh.Points, 1))];
+      x_z_negative_y_face_mesh.ConnectivityList + (size(x_z_positive_y_face_mesh.Points, 1));...
+      y_z_positive_x_face_mesh.ConnectivityList + (size(x_z_positive_y_face_mesh.Points, 1) +...
+                                                  size(x_z_negative_y_face_mesh.Points, 1));...
+      y_z_negative_x_face_mesh.ConnectivityList + (size(x_z_positive_y_face_mesh.Points, 1) +...
+                                                  size(x_z_negative_y_face_mesh.Points, 1) +...
+                                                  size(y_z_positive_x_face_mesh.Points, 1))];
 
   %* --------------- FILTER FOR UNIQUE POINTS --------------- *%
   [GPHS_bricks_mesh_points, ~, IC] = unique(GPHS_bricks_mesh_points, 'rows', 'stable');
   GPHS_bricks_mesh_connectivity = IC(GPHS_bricks_mesh_connectivity);
   %* --------------- STORE THE TRIANGULATION --------------- *%
   GPHS_bricks_mesh = triangulation(GPHS_bricks_mesh_connectivity, GPHS_bricks_mesh_points);
+  GPHS_bricks_mesh = translateMesh( GPHS_bricks_mesh , [ 0 , 0 , -GPHS_STACK_COUNT/2 * GPHS_HEIGHT_z ] );
 
   %* --------------- DUPLICATE BRICKS VERTICALLY --------------- *%
   BRICK_meshes = cell(GPHS_STACK_COUNT,1);
   BRICK_meshes{GPHS_STACK_COUNT, 1} = GPHS_bricks_mesh;
 
-  for i = 1 : (GPHS_STACK_COUNT) - 1
-    BRICK_meshes{GPHS_STACK_COUNT - i, 1} = translateMesh(GPHS_bricks_mesh, [0, 0, i*(GPHS_HEIGHT_z)]);
+  for j = 1 : (GPHS_STACK_COUNT) - 1
+    BRICK_meshes{GPHS_STACK_COUNT - j, 1} = translateMesh(GPHS_bricks_mesh, [0, 0, j*(GPHS_HEIGHT_z)]);
   end
 
   %* --------------- EXPORT MESHES --------------- *%
   if EXPORT_TOGGLE
-    for i = 1 : GPHS_STACK_COUNT
-        stlwrite(BRICK_meshes{i, 1}, output_path + "GPHS_brick-row-" + num2str(i) + "-ref-" + num2str(REFINE_SCALE) + ".stl", "binary")
+    for j = 1 : GPHS_STACK_COUNT
+        stlwrite(BRICK_meshes{j, 1}, output_path + "GPHS_brick-row-" + num2str(j) + ".stl", "binary")
     end
   end
 
-  %* --------------- PLOT MESHES --------------- *%
-  if GRAPH_TOGGLE
-    
-    % figure
-    hold on
-    for i = 1 : GPHS_STACK_COUNT
-      trisurf(BRICK_meshes{i, 1},'EdgeColor',[1,1,1].*mod(i,2),'LineStyle','-','FaceColor',[1,0,0].*i/GPHS_STACK_COUNT)
-    end
-    pbaspect([1,1,1])
-    daspect([1,1,1])
-    xlabel('x')
-    ylabel('y')
-    zlabel('z')
-    view(3)
+end
 
+%* --------------- PLOT MESHES --------------- *%
+if GRAPH_TOGGLE
+  
+  % figure
+  hold on
+  for i = 1 : GPHS_STACK_COUNT
+    trisurf(BRICK_meshes{i, 1},'EdgeColor','none','FaceColor','b','FaceAlpha',0.5)
   end
+  pbaspect([1,1,1])
+  daspect([1,1,1])
+  xlabel('x')
+  ylabel('y')
+  zlabel('z')
+  view(3)
+  hold off
 
 end
